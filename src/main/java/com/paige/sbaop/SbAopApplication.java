f92d7eb002eb4989b31d6f5ac03f05377d757fad
qian.liu <liuqian@3ycloud.com>
2020-12-15 22:51:59 +0800

    创建Spring Boot项目：只引入了必要的包(starter的parent,web,aop,test，后两个后续用到)；编写了启动类和一个简单的controller

---
+++ src/main/java/com/paige/sbaop/SbAopApplication.java
@@ -0,0 +1,15 @@
+package com.paige.sbaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author paige
+ * @create 2020-12-15 22:28
+ */
+@SpringBootApplication
+public class SbAopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SbAopApplication.class, args);
+    }
+}
---
+++ src/main/java/com/paige/sbaop/controller/WebLogTestController.java
@@ -0,0 +1,22 @@
+package com.paige.sbaop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Web日志测试相关接口
+ * @author paige
+ * @create 2020-12-15 22:38
+ */
+@RestController
+@RequestMapping("/weblog")
+public class WebLogTestController {
+
+    @GetMapping("/get-test")
+    public String hello(@RequestParam("name") String name) {
+        System.out.println("Call hello...");
+        return name;
+    }
+}
